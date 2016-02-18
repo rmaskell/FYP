c8ca9f795c7422c20d74f23a69e2872583d9547c
Richard19940 <richard_maskell_1@hotmail.co.uk>
2016-02-18 18:55:10 +0000

    Create temp.php

---
+++ temp.php
@@ -0,0 +1,37 @@
+<?php
+
+function resetPassword($ticket, $emailAddress, $newPassword){
+	//Create query
+	$databaseQuery = "SELECT * FROM login WHERE emailAddress='$emailAddress'";
+	
+	//Execute Database query
+	$result = executeDatabase($databaseQuery);
+	
+	//Fetch array
+	while($row = mysqli_fetch_array($result)){
+
+		//Create ticket based off database
+		$hash = $row['hash'];
+		$password = $row['password'];
+		$checkTicket = $hash.$password;
+		
+		if ($checkTicket == $ticket){
+		  
+		  //Clean query input
+		  $emailAddress = secureStrings($emailAddress);
+		
+			$newPassword = saltPassword($newPassword);
+			$databaseQuery = "UPDATE login SET password='$newPassword' WHERE emailAddress='$emailAddress'";
+			executeDatabase($databaseQuery);
+			print ('reset');
+		}
+		
+		else {
+			print ('brequest');
+		}
+				
+	}
+	
+}
+
+?>
